2d681d3a601743752c8c8a7a93d55d3ddaed365f
Devin Droddy <devin@justdeeevin.dev>
2025-11-22 03:20:50 -0500

    new docs

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -13,15 +13,23 @@ mod termwiz;
 
 use ratatui::Terminal;
 
+/// Some extra functionality that a backend must have for ratatui-elm to work.
 pub trait Backend: ratatui::backend::Backend + Sized {
+    /// The type of event that the backend produces.
     type Event: Event;
+    /// The type of error that the backend produces.
     type Error: std::error::Error;
+    /// An asynchronous stream of events.
     type EventStream: futures::Stream<Item = Result<Self::Event, Self::Error>> + Default + Unpin;
 
+    /// Initialize the backend.
     fn init() -> Terminal<Self>;
+    /// Restore the terminal to its original state.
     fn restore();
 }
 
+/// Specific functionality a backend's event must have for ratatui-elm to work.
 pub trait Event {
+    /// Check if the event is a resize event.
     fn is_resize(&self) -> bool;
 }
